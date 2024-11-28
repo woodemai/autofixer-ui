c5dc030c0353422308bf1d38767b53dfa27a00b3
Nikolay Savchenko <nikolay.savchenko@redcollar.ru>
2024-11-28 03:30:23 +0300

    add types to next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
+import type { NextConfig } from "next";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    reactStrictMode: true,
+} satisfies NextConfig;
 
 export default config;
